71fa72d860aa2f788aa89069159986a451410154
Evan Hsu <lchsu1114@gmail.com>
2025-12-04 15:52:46 +0800

    Modify error.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path'; // 如果你有用到多頁面配置
 
 export default defineConfig({
+  base: '/', // ✅ 絕對要設為根目錄
+  build: {
+    rollupOptions: {
+        input: {
+            main: resolve(__dirname, 'index.html'),
+            // room2: resolve(__dirname, 'room2.html'), // 如果有的話
+        }
+    }
+  },
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
